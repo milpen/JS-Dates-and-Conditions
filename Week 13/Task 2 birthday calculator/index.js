0b9b6cfd0c1cdf9d4d9049525d3629252d0de095
milpen <milpen@mail.ru>
2023-09-03 18:22:28 +0500

    task 2 add text line

--- Week 13/Task 2 birthday calculator/index.js
+++ Week 13/Task 2 birthday calculator/index.js
@@ -4,41 +4,26 @@ const text = document.getElementById('text');
 function calculate(event) {
     event.preventDefault();
     const birthdayDate = inputDate.value;
-    const birthdayDateStamp = Date.parse(birthdayDate);// перевели в таймстемп
-    console.log(1, birthdayDateStamp);
-    const currentTimestamp = Date.now();// текущий таймстемп
-    const timeDiff = birthdayDateStamp - currentTimestamp;   // разница в таймстемпах
-    console.log(2, timeDiff);
+    const birthdayDateStamp = Date.parse(birthdayDate);
+    const currentTimestamp = Date.now();
+    const timeDiff = birthdayDateStamp - currentTimestamp;
     const daysLeft = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-    console.log(3, daysLeft);
+// часть для изменения слова день
+    const daysPronunciation = daysLeft;
+    if (daysLeft % 100 /10 === 0 && daysLeft % 100 /10 === 5 && daysLeft % 100 /10 === 6 && daysLeft % 100 /10 === 7 && daysLeft % 100 /10 === 8 && daysLeft % 100 /10 === 9) {
+        daysPronunciation.textContent = 'дней';
+    } else if (daysLeft % 100 /10 === 2 || daysLeft % 100 /10 === 3 || daysLeft % 100 /10 === 4) {
+        daysPronunciation.textContent = 'дня';
+    } else {
+        daysPronunciation.textContent = 'день';
+    }
 
     if (birthdayDate === '') {
         text.textContent = 'Пожалуйста, введите дату рождения';
         text.style.color = 'red';
     } else {
-        text.textContent = `До вашего дня рождения осталось ${daysLeft} дней`;
+        text.textContent = `До вашего дня рождения осталось ${daysLeft} ${daysPronunciation.textContent}`;
         text.style.color = 'black';
     }
 }
-  // function calculateBMI(event) {
-  //   event.preventDefault();
-  //   const weight =+weightInput.value;
-  //   const height = +heightInput.value / 100;
-  //   const doubledHeight = Math.pow(height, 2);
-  //   const BMI = weight / doubledHeight;
-  //   const finalBMI = BMI.toFixed(2);
-  //   result.innerHTML = finalBMI;
-    
-  //   const p = document.createElement('p');
-  
-  //   if (result.innerHTML < 18.5) {
-  //     p.textContent = "Недостаточный вес"; 
-  //   } else if(result.innerHTML >= 18.5 && result.innerHTML < 24.9) {
-  //     p.textContent = "Нормальный вес";
-  //   } else if(result.innerHTML >=24.9 && result.innerHTML < 29.9) {
-  //     p.textContent = "Избыточный вес";
-  //   }
-  // result.appendChild(p);
-  // }
-  
-  button.addEventListener('click', calculate);
+button.addEventListener('click', calculate);
