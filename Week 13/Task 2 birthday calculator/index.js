388572cbae656929fab993d2bf5c869c8754182d
milpen <milpen@mail.ru>
2023-09-04 21:25:56 +0500

    task 2 done

--- Week 13/Task 2 birthday calculator/index.js
+++ Week 13/Task 2 birthday calculator/index.js
@@ -8,31 +8,16 @@ function calculate(event) {
     const currentTimestamp = Date.now();
     const timeDiff = birthdayDateStamp - currentTimestamp;
     const daysLeft = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-// часть для изменения слова день
-  //  const daysPronunciation = daysLeft;
-/*    if (daysLeft % 10 === 5 && daysLeft % 10 === 6 && daysLeft % 10 === 7 && daysLeft % 10 === 8 && daysLeft % 10 === 9) {
-        daysPronunciation.textContent = 'дней';
-    } else if (daysLeft % 10 === 2 || daysLeft % 10 === 3 || daysLeft % 10 === 4) {
-        daysPronunciation.textContent = 'дня';
-    } else {
-        daysPronunciation.textContent = 'день';
-    }*/
 
     if (birthdayDate === '') {
         text.textContent = 'Пожалуйста, введите дату рождения';
         text.style.color = 'red';
     } else {
-        text.textContent = `До вашего дня рождения осталось ${daysLeft} ВСТАВИТЬ РЕЗУЛЬТАТ ФУНКЦИИ dayName`;
+        text.textContent = `До вашего дня рождения осталось ${daysLeft} ${dayName(daysLeft)}`;
         text.style.color = 'black';
     }
-    return daysLeft;
 }
 function dayName(daysLeft) {
-    /*if (daysLeft > 10 && [11, 12, 13, 14].includes(daysLeft%100)) return 'дней';
-    last_num = daysLeft%10;
-    if (last_num == 1) return 'день';
-    if ([2,3,4].includes(last_num)) return 'дня';
-    if ([5,6,7,8,9, 0].includes(last_num)) return 'дней';*/
     if (daysLeft ===1) {
         return 'день';
     } else if (daysLeft > 1 && daysLeft < 5) {
@@ -40,13 +25,17 @@ function dayName(daysLeft) {
     } else {
         return 'дней';
     }
-
 }
 
 button.addEventListener('click', calculate);
 
-function finalCall(evt) {
+
+//ВОПРОС: я хотела сделать вызов нескольких функций через нажатие на кнопку, т.е. код строки 30 и далее был бы таким, как ниже, но этот вариант у меня работать отказался. Что я сделала не так? (хочу понять, т.к. написать несколько функций и объединить их вызов общим действием, например, нажатием на конпку, это жизненно необходимая вещь)):
+
+/*function finalCall(evt) {
     evt.preventDefault();
     calculate();
     dayName();
 }
+button.addEventListener('click', finalCall); //нажатием на кнопку вызываю финальную функцию, которая запустит функции calculate() и dayName()
+*/
